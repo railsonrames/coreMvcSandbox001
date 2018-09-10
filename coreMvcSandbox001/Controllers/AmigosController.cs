1a564f5380dd682d593ea21644de82256d5f4b2a
Railson Rames <railson@msn.com>
2018-09-10 18:02:13 -0300

    Pontapé

---
+++ coreMvcSandbox001/Controllers/AmigosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreMvcSandbox001.Controllers
+{
+    public class AmigosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public String[] Listar(int id)
+        {
+            String listaConsolidada = "";
+            String[] listaDeAmigos = { "Mateus", "Marquinhos do Pipe", "Tiago Jardel Herter", "Mario Bittar", "Bernardo" };
+            int[] numeros = { 10, 22, 12, 6, 7};
+            Console.WriteLine(numeros.Length);
+            return listaDeAmigos;
+        }
+    }
+}
